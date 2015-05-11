133284117d938de1e8305a683082fba9f7295231
Jennifer B Armstrong <jenbeckham@gmail.com>
2015-05-11 06:57:19 -0400

    Try to make Error class

--- convert_currency.rb
+++ convert_currency.rb
@@ -7,17 +7,18 @@ require "./currency_converter.rb"
 # puts eur.currency_hash
 
 # dollars = Currency.new(7, "USD")
-# euros = Currency.new(8, "EUR")
+euros = Currency.new(8, "EUR")
+# puts Currency.count
 #
 # # rates.convert(Currency.new(7, "CAD"),"EUR")
 #
 # #p shows everything for the command
 #
-# dollarbill = Currency.new(1, "USD")
+dollarbill = Currency.new(1, "USD")
 # buck = Currency.new(1, "USD")
 
 # p dollarbill + euros
-# dollarbill - euros
+dollarbill - euros
 # p dollarbill + buck
 # p dollarbill - dollarbill
 
@@ -26,7 +27,7 @@ require "./currency_converter.rb"
 
 # # puts dollars.amount_money
 # # puts dollars * 0.5
-# hello = Currency.new ("$5")
+# hello = Currency.new ("$5,000.00")
 # p hello.convert_symbol
 # ohno = Currency.new ("€4")
 # p ohno.convert_symbol
--- currency.rb
+++ currency.rb
@@ -1,29 +1,50 @@
+class Error < Exception
+  attr_reader :reason
+  def initialize (reason)
+    @reason = reason
+  end
+end
+#tried to make Error class instead of using if/else in method
+
 class Currency
   attr_reader :code, :amount
+  #allows me to call upon object argument
+  attr_reader :code, :amount
+  #allows me to motify object argument
+  @@count = 0
+  #creates a a variable that is for the whole class allow
+  #  me to keep track of the number of objects in class
   def initialize (amount, code = "USD")
     #need default for object to accept 1 argument
     @code = code #currency code
     @amount = amount #amount
+    @@count +=1
+    #add 1 to count everytime a new object is made for the class
   end
 
   def == (other)
-     @amount == other.amount && @code == other.code
+    @amount == other.amount && @code == other.code
+     #checks equality of class objects
   end
 
-  def + (other)
+  def + (other) #allows addition of class objects
     if @code == other.code
       Currency.new(@amount.to_f + other.amount.to_f, @code)
     else
       p "DifferentCurrencyCodeError"
     end
+      # rescue
+      #   raise Error.new("DifferentCurrencyCodeError")
   end
 
-  def - (other)
+  def - (other)    #allows substraction of class objects
     if @code == other.code
       Currency.new(@amount.to_f - other.amount.to_f, @code)
     else
-      p "DifferentCurrencyCodeError"
+      p"DifferentCurrencyCodeError"
     end
+    #   raise Error.new("DifferentCurrencyCodeError") unless @code == other.code
+    # return Currency.new(@amount.to_f - other.amount.to_f, @code)
   end
 
   def * (other)
@@ -32,9 +53,25 @@ class Currency
 
   def convert_symbol
     symbol = {"€" => "EUR", "$" => "USD"}
-    Currency.new(@amount.gsub(/[^\d\.]/, '').to_f, symbol[@amount.gsub(/[0-9]/, '')])
+    #hash with symbols as keys and codes as values
+    if symbol.has_key?(@amount.gsub(/[0-9.,]/, '') == true
+      Currency.new(@amount.gsub(/[^\d\.]/, ''), symbol[@amount.gsub(/[0-9.,]/, '')])
+    else
+      p "UnknownCurrencyCodeError"
+    # if symbol.has_key?(@amount.gsub(/[0-9.,]/, '') == false
+    #   raise Error, "UnknownCurrencyCodeError"
+    # rescue Error => e
+    #   p e.message
+    # end
+    # Currency.new(@amount.gsub(/[^\d\.]/, ''), symbol[@amount.gsub(/[0-9.,]/, '')])
   end
-end
+    #gsub is a global substitute.
+    #/[^\d\.]/ means sub everything but digits(d) and . with ''
+    #/[0-9]/ means sub 0-9 characters with ''
+    #need to find way to only take in "," for "." in other countries currencies
 
-class MyCrazyError < StandardError
+  def self.count
+    return @@count
+  end
+  #something I was playing with
 end
